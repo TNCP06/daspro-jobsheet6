7eb9d0a24d3159a9ae72ba04a21659534b85f682
TNCP06 <ctionusa6@gmail.com>
2023-10-03 15:46:43 +0700

    Modifikasi1 Percobaan 1

--- Pemilihan2Percobaan127.java
+++ Pemilihan2Percobaan127.java
@@ -11,9 +11,13 @@ public class Pemilihan2Percobaan127 {
         if ( (tahun % 4) == 0 ) {
             if ( (tahun % 100) != 0 ) {
                 System.out.println("Tahun Kabisat!");
+            } else {
+                System.out.println("Bukan Tahun Kabisat!");
             }
         } else {
             System.out.println("Bukan Tahun Kabisat!");
         }
+
+        input27.close();
     }
 }
